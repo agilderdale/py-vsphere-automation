74bf31fea9be88811bff28969731b83a206bebaf
agilderdale <agilderdale@vmware.com>
2020-07-11 17:56:50 +0100

    fighting with

--- menu.sh
+++ menu.sh
@@ -228,7 +228,10 @@ f_update_config_file1() {
     egrep -v ^'(#|$)' /tmp/config
 
     while read line; do
-    case "$line" in \#*) sed -i -e '/^[[:space:]]*$/d' /tmp/config ; sed -i -e '/^#/d' /tmp/config; continue ;; esac
+    case "$line" in
+        \#*) sed -i -e '/^[[:space:]]*$/d' /tmp/config ; sed -i -e '/^#/d' /tmp/config; continue ;;
+        \-*) sed -i -e '/-/d' /tmp/config; continue;; esac
+
       var1=`echo $line`
       echo $var1
       if [[ ! -z "$var1" ]] ; then
